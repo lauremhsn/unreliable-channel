aadf48d1d9421d71bfd6cd47de23e0d12cc0a88b
Rola <144335560+rmd35@users.noreply.github.com>
2024-11-21 21:41:17 +0200

    Update UnreliableChannel.java

--- UnreliableChannel.java
+++ UnreliableChannel.java
@@ -1,37 +1,45 @@
 import java.net.*;
 import java.util.*;
 
-public class UnreliableChannel{ //port, probability, minD, maxD
+public class UnreliableChannel{ //port, probability, minimum delay, maximum delay
     //take in port
     //receive packet
     //random to simulate packet loss and delay (do we keep or not)
     public static void main (String [] args) throws Exception{
         int port = Integer.parseInt(args[0]);
+        double probs = Double.parseDouble(args[1]);
+        int minD = Integer.parseInt(args[2]);
+        int maxD = Integer.parseInt(args[3]);
         byte[] anotherBuff = new byte[1024];
 
-
+        
         DatagramSocket theSocketII = new DatagramSocket(port);
         DatagramPacket thePacketII = new DatagramPacket(anotherBuff, anotherBuff.length);
-        double probs = 0.3; //loss p
+        
+        //counters
+        int counterOfPackets = 0;
 
         int loserA = 0;
         int keeperA = 0;
+        int delayA = 0;
 
         int loserB = 0;
         int keeperB = 0;
-
-        int loser = 0;
-        int keeper = 0;
-
+        int delayB = 0;
 
         while(true){
             theSocketII.receive(thePacketII);
+
             String str = new String(thePacketII.getData());
+            if (str.equals("END")) {
+                break; 
+            }
 
-
-
+            counterOfPackets++; 
 
             Random rnd = new Random();
+            int packetDelay = rnd.nextInt(minD, maxD + 1);
+
             if (rnd.nextDouble()<=probs){
                 if (str.charAt(0) == 'A'){
                     ++loserA;
@@ -41,33 +49,30 @@ public class UnreliableChannel{ //port, probability, minD, maxD
                 }
                 continue;
             }
-            else{
+            else {
+                Thread.sleep(packetDelay);
+
                 if (str.charAt(0) == 'A'){
                     ++keeperA;
+                    delayA += packetDelay;
                 }
                 else{
                     ++keeperB;
+                    delayB += packetDelay;
                 } 
             }
 
-
-
-
-            if (new String(thePacketII.getData()) == "END") {
-                //counters
-                break;
-            }
-            /*if (str.charAt(0) == 'A'){
-                loserA = loser;
-                keeperA = keeper;
-            }
-            else{
-                loserB = loser;
-                keeperB = keeper;
-            }*/
         }
+        double averageDelayA = 0, averageDelayB = 0;
+        if (keeperA > 0)
+            averageDelayA = delayA / keeperA;
+        if (keeperB > 0)
+            averageDelayB = delayB / keeperB;
+        System.out.println("Packets delivered from user A: " + counterOfPackets + " | Lost: " + loserA + " | Delayed: " + keeperA);
+        System.out.println("Packets delivered from user B: " + counterOfPackets + " | Lost: " + loserB + " | Delayed: " + keeperB);
+        System.out.println("Average delay from A to B: " + averageDelayA);
+        System.out.println("Average delay from A to B: " + averageDelayB);
 
-
-        
+        theSocketII.close();
     }
 }
